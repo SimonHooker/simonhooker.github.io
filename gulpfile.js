e0165a6f4c9a8dfa7144d9c1f73f7a1e9ba6764b
Simon Hooker <simon@strayegg.com>
2015-05-03 21:30:17 +0100

    Basic homepage style ready ish for colouring etc

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp 	= require('gulp'),
 	less 	= require('gulp-less'),
 	csso 	= require('gulp-csso'),
-	concat 	= require('gulp-concat');
+	concat 	= require('gulp-concat')
+	watch	= require( 'gulp-watch');
+
+var css_source = './stylesheet/strayegg.less';
 
 gulp.task('css', function () {
-	return gulp.src('./stylesheet/strayegg.less')
+	return gulp.src( css_source )
 		.pipe(less())
 		.pipe(csso())
 		.pipe(concat('strayegg.css'))
@@ -13,4 +16,8 @@ gulp.task('css', function () {
 
 gulp.task('default', ['css'], function () {
 	return;
+});
+
+gulp.task('watch',function(){
+	gulp.watch( css_source , [ 'css' ] );
 });
