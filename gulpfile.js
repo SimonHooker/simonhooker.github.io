e78da8ef51ec8b735b88f7c4b26193a6e52c484f
Simon Hooker <simon@mso.net>
2015-05-11 13:54:49 -0500

    Modifying css minification to be mini-er

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,17 @@
-var gulp 	= require('gulp'),
-	less 	= require('gulp-less'),
-	csso 	= require('gulp-csso'),
-	concat 	= require('gulp-concat')
-	watch	= require( 'gulp-watch');
+var gulp 		= require('gulp'),
+	less 		= require('gulp-less'),
+	minifyCss 	= require('gulp-minify-css'),
+	concat 		= require('gulp-concat'),
+	watch		= require('gulp-watch'),
+	uncss		= require('gulp-uncss');
 
 gulp.task('css', function () {
 	return gulp.src( './stylesheet/_src/strayegg.less' )
 		.pipe(less())
-		.pipe(csso())
+		.pipe(uncss({
+			html: ['_site/**/*.html']
+		}))
+		.pipe(minifyCss())
 		.pipe(concat('strayegg.css'))
 		.pipe(gulp.dest('./stylesheet/'));
 });
